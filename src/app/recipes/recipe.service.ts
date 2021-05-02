e4fe21c52580011895966f43b161890b38d8a933
RussellLin <superbr7789@gmail.com>
2021-05-02 18:25:01 +0800

    feat: add shopping list functionalize with correct recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
 import { Recipe } from './recipe.model';
 
@@ -7,12 +9,32 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>()
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply another test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+      ])
   ];
+
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     // 因為不想直接回拋 service 裡的 recipe 陣列，所以運用 slice() 讓函式回傳 copy 的陣列
     return this.recipes.slice()
+  }
+
+  onAddShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredientsToShoppingList(ingredients)
   }
 }
