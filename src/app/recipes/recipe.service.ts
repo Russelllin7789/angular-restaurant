39b2f07af05d6eee42477d0478e1ca9035b23f9c
RussellLin <superbr7789@gmail.com>
2021-05-10 00:54:13 +0800

    feat: add route control

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -34,6 +34,10 @@ export class RecipeService {
     return this.recipes.slice()
   }
 
+  getRecipe(id: number) {
+    return this.recipes[id]
+  }
+
   onAddShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredientsToShoppingList(ingredients)
   }
